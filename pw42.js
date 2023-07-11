443f5d2ac6181cf838ca5c78f118902691c79cb9
ankitsharma97 <ankit972125@gmail.com>
2023-07-12 00:27:30 +0530

    adding new

---
+++ pw42.js
@@ -0,0 +1,39 @@
+// Hello Word
+console.log(this)
+// value and data type
+let a=5;
+let MyName="ankit";
+console.log(MyName)
+
+
+
+
+let highScore=300
+console.log(highScore)
+
+
+let pwSkills=3500
+console.log(pwSkills);
+
+
+highScore=500
+console.log(highScore)
+
+console.log(5+5);
+
+let coursePrice=200;
+let gst=36;
+let final=coursePrice+gst;
+console.log("final amout",final);
+console.log(3**2);
+
+console.log(5<4);
+
+
+let value1=true;
+let value2=false;
+let value3=true;
+let value4=false
+
+
+console.log(value1 ^ value3);
