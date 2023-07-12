7063720cc2944abebd36daea10c113832e648505
ankitsharma97 <ankit972125@gmail.com>
2023-07-13 00:08:15 +0530

    adding new

---
+++ pw43.js
@@ -0,0 +1,74 @@
+// conditions
+
+
+// let age=19;
+// if(age>=18)
+// {
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you can not vote");
+// }
+
+// let signal='';
+// if(signal=='red')
+// {   
+//     console.log("stop");
+// }
+// else if(signal=='yellow')
+// {
+//     console.log("ready");
+// }
+// else if(signal=='green')
+// {
+//     console.log("go");
+// }
+// else{
+//     console.log("wrong signal");
+// }
+
+//switch case
+// let user='admin';
+// switch (user) {
+//     case '':
+//         console.log("you can do anything");
+//         break;
+//     case 'visiter':
+//         console.log("just enjoy");
+//     default:
+//         console.log("spam");
+//         break;
+// }
+
+
+
+
+// loops
+
+// for loop
+// let a=5;
+// for(let i=1;i<11;i++)
+// {
+//     console.log(a*i);
+// }
+
+//while loop
+// let i=5;
+// while (i!=0) {
+//     console.log(i);
+//     i--;
+// }
+
+//do-while
+// let a=0;
+// do{
+//     console.log(a);
+// }while(a>0);
+
+// ternary=(condition?true:false)
+// let a=5;
+// console.log(a>7?"yes":"no");
+
+
+// let a;
+// console.log(a);
