0d2df99b778ce36c4dde3b1e7479ffaa5f966968
ankitsharma97 <ankit972125@gmail.com>
2023-07-14 15:05:05 +0530

    adding new

---
+++ cn_js01.js
@@ -0,0 +1,13 @@
+// automatic coversation
+console.log("hello" + "world");
+console.log("1"-1);
+
+// 
+console.log(1=="1");
+console.log(1==="1");
+
+// function
+function showAlert(){
+    alert("hey");
+}
+showAlert();
